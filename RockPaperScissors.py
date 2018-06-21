d718c50a1d5c1dd8c5803ab9bcd6cea43bb5df35
Sabaoon Raza Khan <Sabaoon96@users.noreply.github.com>
2018-06-21 13:30:41 +1200

    Updated RockPaperScissors file

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -9,18 +9,19 @@ scissors = 3
 user = 0
 
 while user != -1:
-    print("Choose \n1 for Rock, \n2 for Paper, \n3 for Scissors, \nor \n-1 to Quit!\n")
+    print("Choose: \n1 for Rock, \n2 for Paper, \n3 for Scissors, \nor \n-1 to Quit!\n")
     user = int(input("Please enter your choice: "))
     computerChoice = random.randint(1,3)
 
     if user != -1:
         if user == rock:
-              print("You chose rock")
+              print("\nYou chose rock")
         elif user == paper:
-              print("You chose paper")
+              print("\nYou chose paper")
         elif user == scissors:
-              print("You choose scissors")
+              print("\nYou choose scissors")
     else:
+        print("You Quit the Game :(")
         break
 
     
@@ -34,8 +35,11 @@ while user != -1:
 
     if (computerChoice == user):
         print("It's a TIE!!!")
+        print("~"*20, "\n")
     elif (user == rock and computerChoice == scissors) or (user == paper and computerChoice == rock) or (user == scissors and computerChoice == paper):
-        print("You win :)")        
+        print("You win :)")
+        print("~"*20, "\n")
     else:
         print("The computer won :(")
+        print("~"*20, "\n")
         
